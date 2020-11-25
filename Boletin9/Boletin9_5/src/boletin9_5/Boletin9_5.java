8bf39af5004d030e013f5b7078f6fa432f41ab89
Manuel Places Alvarez <72249023+mplacesalvarez@users.noreply.github.com>
2020-11-25 14:35:56 +0100

    Add files via upload

--- Boletin9/Boletin9_5/src/boletin9_5/Boletin9_5.java
+++ Boletin9/Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -1,4 +1,5 @@
 
+
 package boletin9_5;
 
 import java.util.Scanner;
@@ -6,27 +7,18 @@ import java.util.Scanner;
 public class Boletin9_5 {
 
     public static void main(String[] args) {
-        int i;
-        float salarios[] = null;
+        float salario;
+      Scanner obxsc=new Scanner(System.in);
+      Salario obx=new Salario();
+      
+        do{System.out.println("Introduce un salario: ");
+      salario=obxsc.nextFloat();
+        obx.salario=salario;obx.numero_traballadores();
+      obx.porcentaxe_salario();
+        } while (salario!=0);
+        
+     obx.mensajes();
+    
+}   
 
-        System.out.println("Introduce los sueldos:");
-        Scanner obxsc=new Scanner(System.in);
-       
-           for(i=0; i>0 ; i++){if (salarios[i] >0) {salarios[i]=obxsc.nextFloat();}else{System.out.println("No se aceptan números negativos");}
-          if(salarios[i]==0)break;
-          }
-    
-           
-    Salario obx=new Salario(salarios[i],i);
-    
-    
-    
-    
-    
-    obx.numero_traballadores();
-    obx.porcentaxe_salario();
-        
-        
-}   }
-   
-    
+}
--- Boletin9/Boletin9_5/src/boletin9_5/Salario.java
+++ Boletin9/Boletin9_5/src/boletin9_5/Salario.java
@@ -1,28 +1,46 @@
 package boletin9_5;
 
 
+
+import java.util.Scanner;
+
 public class Salario {
-    float salario; int contador_massueldo, contador_menossueldo, i,j;
+    float salario; int contador_massueldo=0, contador_menossueldo=0; int porcentaxe;
    
+    Scanner obxsc=new Scanner(System.in);
+  //  public Salario(float salario){
+ //this.salario=salario;
     
-    public Salario(float salario, int i){
- this.salario=salario;
- this.i=i;
- }
-    
+   // }
     
   public void numero_traballadores(){
-    for(j=0; j==i; j++){if((salario>=1000)&&(salario<=1750)){contador_massueldo++;}}
-    System.out.println(contador_massueldo+" trabajadores ganan entre 1000 y 1750");
-   
-    }
+    if((salario>=1000)&&(salario<=1750)){contador_massueldo++;}
+    
+  }
+    
     
     public void porcentaxe_salario(){
-    for(j=0; j==i; j++){if(salario<1000){contador_menossueldo++;}}
-    int porcentaxe=(contador_menossueldo*i)/100;
-    System.out.println(porcentaxe+"% de los trabajadores ganan menos de 1000");
+    if(salario>=1&&salario<1000){contador_menossueldo++;
+    porcentaxe=(contador_menossueldo*100/(contador_menossueldo+contador_massueldo));
+  
+
+    
+    
     }
     
-}
+    }
+    
+    
+    public void mensajes(){
+    System.out.println(contador_massueldo+" trabajadores ganan entre 1000 y 1750");
+      System.out.println(porcentaxe+"% de los trabajadores ganan menos de 1000");
+    }
+    
+    
+    
+    
+    
+    }
+
 
     
