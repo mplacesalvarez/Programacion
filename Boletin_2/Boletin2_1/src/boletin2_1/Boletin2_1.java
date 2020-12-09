a78ab226c71cd59f3822eeac3637952d69270a83
Manuel Places Alvarez <72249023+mplacesalvarez@users.noreply.github.com>
2020-12-09 13:59:29 +0100

    Add files via upload

---
+++ Boletin_2/Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,16 @@
+
+package boletin2_1;
+
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+      float base=4, altura=3;
+    
+        float area= (base*altura)/2;
+                
+       System.out.println("El área es: "+area);
+        
+    }
+    
+}
